8617aff32847452702456a743a0bc0b835cff464
Your Name <you@example.com>
2024-11-20 15:56:22 -0400

    still working on

--- A3-P3-GirlGuideCookies.py
+++ A3-P3-GirlGuideCookies.py
@@ -4,19 +4,16 @@
 #               those guides who go above and beyond in their sales efforts. The organizers
 #               want a program to print the guide list and their prizes.
 
-#Student #:     
-#Student Name:  
+#Student #:     W0239497
+#Student Name:  Chauntel Smith
 # YOUR CODE STARTS HERE, each line must be indented (one tab)
-#Ask the user how many guides sold cookies.
-# def GGuideStats():
-
-#Ask the user how many guides sold cookies.
-# def GGuideStats():
-nameList = []
-soldBoxes = []
-gGuideStats = int(input("Enter number of Guides that participated in event: "))
+#Lists
+# nameList = []
+# soldBoxes = []
+# gGuideStats = int(input("Enter number of Guides that participated in event: "))
 
 #Calculate and output a list of all guide names.
+
 def giudeInfo(nameList, soldBoxes, gGuideStats):
     for i in range(gGuideStats):
         GgName = input(f"Enter the name of guide #{i + 1}: ")
@@ -25,42 +22,47 @@ def giudeInfo(nameList, soldBoxes, gGuideStats):
         soldBoxes.append(box)
     return nameList,soldBoxes
 
-def totalBox(soldBoxes):
+#Total calculations
+def totalBox(soldBoxes,nameList):
     totalSoldBox = sum(soldBoxes)
-    return totalSoldBox
+    average = totalSoldBox / len(soldBoxes)
+    maxWinner = max(soldBoxes)
+    indexOfMax = soldBoxes.index(len(nameList))
+    return totalSoldBox,indexOfMax,maxWinner,average
 
+#average calculations
 
-def averageBoX(soldBoxes, totalSoldBox):
-    averageSold = totalSoldBox / soldBoxes
-    return averageSold
+# #Prizes calculations
+def prizeCalculations(soldBoxes,nameList,average,indexOfMax):
+    winnerList = []
+    for i in range(len(nameList)):
+        if i == indexOfMax:
+            winnerList.append(f"{nameList [i]}          -A trip to the Girl Guide Jamboree")
+        elif soldBoxes[i] > average:
+            winnerList.append(f"{nameList [i]}          -Super Seller Badge")
+        elif soldBoxes[i] > 0:
+            winnerList.append(f"{nameList [i]}          -Left over cookies!")
+        else:
+            winnerList.append(f"{nameList [i]}          -")
+    return winnerList
 
-#Prizes 
-firstPrize = print("A tirp to the Girl Guide Jamboree!")
-aAPrize = print("Above Average Badge!")
-effortP = print("left over cookies!")
-tryHarder = print("nothing")
-maxWinner = soldBoxes.index(len(nameList))
-
-#Winner calculations 
-guideName, boxNum = giudeInfo
-totalAmount = totalBox
-averageAmount = averageBoX
-
-for i in range(gGuideStats):
-    if soldBoxes == maxWinner:
-        print(firstPrize)
-    elif soldBoxes > averageAmount:
-        print(aAPrize)
-
-
-
-
-
-#Winner calculations 
-
-
-
-#Calculate the average sales.
+def main():
+    nameList = []
+    soldBoxes = []
+    #Loop to restart if the user imputs the wrong values
+    while True: 
+        gGuideStats = int(input("Enter number of Guides that participated in event: "))
+        nameList,soldBoxes, gGuideStats = giudeInfo(nameList, soldBoxes, gGuideStats)
+        print(giudeInfo(nameList, soldBoxes, gGuideStats))
+        indexOfMax,average = totalBox(soldBoxes,nameList)
+        winnerList = prizeCalculations(soldBoxes,nameList,average,indexOfMax)
+        correct = input("were the your inputs entered correctly? (Y/N): ")
+        if correct.upper() == "Y":
+            print(winnerList)   #Come back soon message
+            break
+        else:
+            print("Sorry about that, lets try again!") 
+main()
 
 #Determine and display the prize each guide won based on their sales.
     # YOUR CODE ENDS HERE
